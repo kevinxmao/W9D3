4647f0b89815011eaba961eb6e66740c14d4ed31
Kevin <kevinmao.xy@gmail.com>
2021-02-24 14:31:23 -0500

    make setUpBoard

--- view_file/src/index.js
+++ view_file/src/index.js
@@ -1,6 +1,12 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view")
+const Game = require("../../tictactoe/game")
 
   $(() => {
     // Your code here
-  });
+    console.log("DOM loaded")
+    const game = new Game();
+    const $el = $(".ttt");
+
+    new View(game, $el);
+
+  }); 
--- view_file/src/ttt-view.js
+++ view_file/src/ttt-view.js
@@ -1,11 +1,29 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard()
+  }
 
-  bindEvents() {}
+  bindEvents() {
+    
+  }
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $("<ul></ul>");
+    this.$el.append($ul);
+
+    // if ($li.attr("class") === "unclicked") {
+      
+    // }
+
+    for (let i = 1; i <= 9; i++) {
+      let $li = $("<li></li>");
+      $ul.append($li);
+    }
+  }
 }
 
 module.exports = View;
